fe39304bba8ca9b4996f70abe2bcdb85014a9dc0
ajshan23 <ajmalshahan23@gmail.com>
2024-12-18 14:25:17 +0530

    hosting route set

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ app.use(cors());
 //routes
 app.use("/api/product", productRouter);
 app.use("/api/auth", authRouter);
-
+app.get("*", (req, res) => {
+    res.sendFile('/var/www/evee/dsa/dsa-frontend/dist/index.html');
+});
 // Start server with db connection
 connectDb().then(() => {
     app.listen(5000, () => console.log('Server running on port 5000'));
